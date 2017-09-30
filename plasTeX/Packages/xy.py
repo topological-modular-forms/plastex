6a8f182596b843790ed095c96428c52d0dfccfea
Raymond Cheng <chengraymond@live.com>
2017-09-29 23:42:13 -0400

    Spacing variants for xypic.

--- plasTeX/Packages/xy.py
+++ plasTeX/Packages/xy.py
@@ -3,8 +3,44 @@
 from plasTeX import NoCharSubCommand, Command
 
 class xymatrix(NoCharSubCommand):
-  args = 'str'
+  args = '{ str }'
 
   class EndRow(Command):
     """ End of a row """
     macroName = '\\'
+
+  def preArgument(self, arg, tex):
+      # Check whether there are any spacing arguments for xymatrix
+      self.spacingArgs = []
+      for t in tex.itertokens():
+        if t == '{':
+          tex.pushToken(t)
+          break
+        else:
+          self.spacingArgs.append(t)
+      super(xymatrix, self).preArgument(arg, tex)
+
+  def postArgument(self, arg, value, tex):
+      self.argSource = "".join(self.spacingArgs) + self.argSource
+      super(xymatrix, self).postArgument(arg, value, tex)
+
+# These are variants of the xymatrix command in which some spacing parameters
+# are specified before the main argument.
+# See section 3.3 of the "XY-pic User's Guide'.
+class xymatrixR(xymatrix):
+  macroName = "xymatrix@R"
+
+class xymatrixC(xymatrix):
+  macroName = "xymatrix@C"
+
+class xymatrixM(xymatrix):
+  macroName = "xymatrix@M"
+
+class xymatrixW(xymatrix):
+  macroName = "xymatrix@W"
+
+class xymatrixH(xymatrix):
+  macroName = "xymatrix@H"
+
+class xymatrixL(xymatrix):
+  macroName = "xymatrix@L"
