9f7bb505298e7249cd314b74cc9ee640f1bca595
Pieter Belmans <pieterbelmans@gmail.com>
2017-10-08 09:20:16 +0200

    Possible fix for #15
    At least it fixes the wrong numbering of theorem-like environments.

--- plasTeX/__init__.py
+++ plasTeX/__init__.py
@@ -1594,6 +1594,8 @@ class TheCounter(Command):
         # In this case, whenever a counter value comes out as a zero,
         # just hank it out.  This is especially useful in document classes
         # such as book and report which do this in the \thefigure format macro.
-        t = re.sub(r'\b0[^\dA-Za-z]+', r'', t)
+
+        # REMARK: disabled this for Gerby
+        #t = re.sub(r'\b0[^\dA-Za-z]+', r'', t)
 
         return tex.textTokens(t)
